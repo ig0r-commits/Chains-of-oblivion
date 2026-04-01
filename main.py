b02be7413aba8cbe04aff89e56a6e74c74c8d295
ig0r-commits <igorkozich2011@gmail.com>
2026-04-01 16:48:40 +0300

    Добавлена статичная карта. Пока что игрок может ходить сквозь стены

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from settings import *
 from player import Player
+from level import LEVEL_MAP
 
 class Game:
     def __init__(self):
@@ -37,9 +38,22 @@ class Game:
                     self.player.move(dy=1)
 
     def draw(self):
-        self.screen.fill(GREY)  # Закрашиваем фон
-        self.player.draw(self.screen)  # Рисуем игрока
-        pygame.display.flip()  # Обновляем экран
+        self.screen.fill(BGCOLOR)
+
+        # Рисуем карту
+        for row_index, row in enumerate(LEVEL_MAP):
+            for col_index, char in enumerate(row):
+                if char == '1':
+                    # Рисуем стену
+                    pygame.draw.rect(self.screen, DARKGREY,
+                                     (col_index * TILESIZE, row_index * TILESIZE, TILESIZE, TILESIZE))
+                else:
+                    # Рисуем пол
+                    pygame.draw.rect(self.screen, (35, 35, 40),
+                                     (col_index * TILESIZE, row_index * TILESIZE, TILESIZE, TILESIZE), 1)
+
+        self.player.draw(self.screen)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     game = Game()
